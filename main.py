76e5ac4aaf658dd04a149086a77c7737dd07522e
maulanamaib <83963442+maulanamaib@users.noreply.github.com>
2023-10-09 19:42:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-'''import pandas as pd
+st.code('''import pandas as pd
 from sklearn.decomposition import LatentDirichletAllocation
 data = pd.read_csv("https://raw.githubusercontent.com/maulanamaib/topic_modelling/master/datavcm.csv")
 del(data['Unnamed: 0'])
-data'''
+data''', language='python')
