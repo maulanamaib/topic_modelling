43045fd96b8052f04ed61dc9cead3fcdcb58a2bd
maulanamaib <83963442+maulanamaib@users.noreply.github.com>
2023-10-09 19:26:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-'''import pandas as pd
+import pandas as pd
 from sklearn.decomposition import LatentDirichletAllocation
 data = pd.read_csv("/content/drive/MyDrive/ppw/datavcm1.csv")
 del(data['Unnamed: 0'])
-data'''
+data
